9df8b889b86ef8a258fc0fa6b01036a6bdc06740
Lord Xar <Lord Xar>
2017-02-01 13:58:06 -0600

    git practice 1.0

---
+++ src/GitPractice.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+//This is a simple program for practicing git version control
+public class GitPractice {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hello, User!");
+	}
+
+}
